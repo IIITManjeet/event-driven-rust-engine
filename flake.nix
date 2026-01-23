baeadb073835bc1a6c1979e988633e8b8b307b02
Manjeet Pathak <johndoe@Manjeet-Pathak-L4R7M456LW.com>
2026-01-24 00:28:17 +0530

    feat:initial-folder-strcture

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Rust Event-Driven Trading System";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.rustfmt
+            pkgs.clippy
+            pkgs.openssl
+            pkgs.pkg-config
+          ];
+
+          shellHook = ''
+            echo "🚀 Rust Event-Driven Trading Dev Shell Ready"
+          '';
+        };
+      }
+    );
+}
